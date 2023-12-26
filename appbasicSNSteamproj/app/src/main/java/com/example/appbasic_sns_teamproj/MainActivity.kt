c5cd8b61ed645ca6060003047a21d38dc038002c
jericho31 <a@a.a>
2023-12-26 15:54:01 +0900

    init

---
+++ appbasicSNSteamproj/app/src/main/java/com/example/appbasic_sns_teamproj/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.appbasic_sns_teamproj
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
