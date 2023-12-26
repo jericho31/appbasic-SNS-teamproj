8d3f77aef9b13a6c72201deb1eb748eb8a79122f
jericho31 <a@a.a>
2023-12-26 15:58:56 +0900

    액티비티 추가

---
+++ appbasicSNSteamproj/app/src/main/java/com/example/appbasic_sns_teamproj/MainPageActivity.kt
@@ -0,0 +1,11 @@
+package com.example.appbasic_sns_teamproj
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainPageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_page)
+    }
+}
