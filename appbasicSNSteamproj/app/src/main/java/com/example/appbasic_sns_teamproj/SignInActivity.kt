190468f4b4cea3928340665094abcf59a0517633
OhSeongWon <osuing1243@daum.net>
2023-12-26 17:45:50 +0900

    로그인 화면, 버튼 클릭시 화면 이동 완성

--- appbasicSNSteamproj/app/src/main/java/com/example/appbasic_sns_teamproj/SignInActivity.kt
+++ appbasicSNSteamproj/app/src/main/java/com/example/appbasic_sns_teamproj/SignInActivity.kt
@@ -12,28 +12,31 @@ class SignInActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
 
-        val editTv_id = findViewById<EditText>(R.id.editTv_id).text.toString()
-        val editTv_pw = findViewById<EditText>(R.id.editTv_pw).text.toString()
+
 
         val btn_goSignUpActivity = findViewById<Button>(R.id.btn_goSignUpActivity)
 
         btn_goSignUpActivity.setOnClickListener {
-
-            val btn_logIn = findViewById<Button>(R.id.btn_logIn)
-
-            if(editTv_id.isEmpty()){
-                Toast.makeText(this, "아이디를 확인해주세요", Toast.LENGTH_SHORT).show()
-            }
-            else if(editTv_pw.isEmpty()){
-                Toast.makeText(this, "비밀번호를 확인해주세요", Toast.LENGTH_SHORT).show()
-            }
-            else{
-                val intent = Intent(this, MainPageActivity::class.java)
-                startActivity(intent)
-            }
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
         }
 
 
+        val btn_logIn = findViewById<Button>(R.id.btn_logIn)
+         btn_logIn.setOnClickListener {
+             val editTv_id = findViewById<EditText>(R.id.editTv_id).text.toString()
+             val editTv_pw = findViewById<EditText>(R.id.editTv_pw).text.toString()
+             if(editTv_id.isEmpty()){
+                 Toast.makeText(this, "아이디를 확인해주세요", Toast.LENGTH_SHORT).show()
+             }
+             else if (editTv_pw.isEmpty()){
+                 Toast.makeText(this, "패스워드를 확인해주세요", Toast.LENGTH_SHORT).show()
+             }
+             else{
+                 val intent = Intent(this, MainPageActivity::class.java)
+                 startActivity(intent)
+             }
 
+         }
     }
 }
