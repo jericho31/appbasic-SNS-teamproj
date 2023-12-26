4ed2e499a35ac1d6cf639e0737e99b1d66ab55d4
OhSeongWon <osuing1243@daum.net>
2023-12-26 17:23:32 +0900

    과제 수정1

--- appbasicSNSteamproj/app/build.gradle.kts
+++ appbasicSNSteamproj/app/build.gradle.kts
@@ -10,7 +10,6 @@ android {
     defaultConfig {
         applicationId = "com.example.appbasic_sns_teamproj"
         minSdk = 26
-        targetSdk = 33
         versionCode = 1
         versionName = "1.0"
 
--- appbasicSNSteamproj/app/src/main/java/com/example/appbasic_sns_teamproj/SignInActivity.kt
+++ appbasicSNSteamproj/app/src/main/java/com/example/appbasic_sns_teamproj/SignInActivity.kt
@@ -1,11 +1,39 @@
 package com.example.appbasic_sns_teamproj
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class SignInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+
+        val editTv_id = findViewById<EditText>(R.id.editTv_id).text.toString()
+        val editTv_pw = findViewById<EditText>(R.id.editTv_pw).text.toString()
+
+        val btn_goSignUpActivity = findViewById<Button>(R.id.btn_goSignUpActivity)
+
+        btn_goSignUpActivity.setOnClickListener {
+
+            val btn_logIn = findViewById<Button>(R.id.btn_logIn)
+
+            if(editTv_id.isEmpty()){
+                Toast.makeText(this, "아이디를 확인해주세요", Toast.LENGTH_SHORT).show()
+            }
+            else if(editTv_pw.isEmpty()){
+                Toast.makeText(this, "비밀번호를 확인해주세요", Toast.LENGTH_SHORT).show()
+            }
+            else{
+                val intent = Intent(this, MainPageActivity::class.java)
+                startActivity(intent)
+            }
+        }
+
+
+
     }
 }
