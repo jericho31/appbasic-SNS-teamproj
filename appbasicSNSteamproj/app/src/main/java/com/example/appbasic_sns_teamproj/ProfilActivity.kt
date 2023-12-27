fb91bf8fd7e0bb9324bec63a6cce6d6ed6897dee
kiwon kim <rlarldnjs17@gmail.com>
2023-12-27 18:56:07 +0900

    프로필 페이지 코드개선

--- appbasicSNSteamproj/app/src/main/java/com/example/appbasic_sns_teamproj/ProfilActivity.kt
+++ appbasicSNSteamproj/app/src/main/java/com/example/appbasic_sns_teamproj/ProfilActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.content.res.ResourcesCompat
 
 class ProfilActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,7 +16,7 @@ class ProfilActivity : AppCompatActivity() {
         val tv_track = findViewById<TextView>(R.id.txt_track)
         val tv_id = findViewById<TextView>(R.id.txt_id)
         val btnLanguageSetting = findViewById<Button>(R.id.btn_languageSetting)
-        val backToMain = findViewById<ImageView>(R.id.iv_return)
+        val backToMainPage = findViewById<ImageView>(R.id.iv_return)
 
         if (intent.hasExtra("track")) {
             tv_track.text = "트랙 : "+ intent.getStringExtra("track")
@@ -24,12 +25,21 @@ class ProfilActivity : AppCompatActivity() {
             tv_id.text = "아이디 : "+ intent.getStringExtra("id")
         }
 
+        val iv_profil = findViewById<ImageView>(R.id.iv_profil)
+        val image = when (tv_track) {
+            android ->
+            ios ->
+            unity ->
+            else ->
+        }
+        iv_profil.setImageDrawable(ResourcesCompat.getDrawable(resources, image, null))
+
         btnLanguageSetting.setOnClickListener{
             val intent = Intent(this, LanguageSettingActivity::class.java)
             startActivity(intent)
         }
 
-        backToMain.setOnClickListener{
+        backToMainPage.setOnClickListener{
             finish()
         }
 
