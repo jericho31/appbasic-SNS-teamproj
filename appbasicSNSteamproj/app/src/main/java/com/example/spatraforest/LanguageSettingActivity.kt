ab007c31bdb492c80dd57e1aeec947a3c73211b3
kiwon kim <rlarldnjs17@gmail.com>
2023-12-27 10:23:00 +0900

    프로필 화면 & 언어 설정 화면

---
+++ appbasicSNSteamproj/app/src/main/java/com/example/spatraforest/LanguageSettingActivity.kt
@@ -0,0 +1,23 @@
+package com.example.spatraforest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.RadioButton
+
+class LanguageSettingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_language_setting)
+
+        val back = findViewById<ImageButton>(R.id.ibtn_back)
+        val korean = findViewById<RadioButton>(R.id.rbtn_korean)
+
+        back.setOnClickListener {
+            finish()
+        }
+
+
+
+    }
+}
