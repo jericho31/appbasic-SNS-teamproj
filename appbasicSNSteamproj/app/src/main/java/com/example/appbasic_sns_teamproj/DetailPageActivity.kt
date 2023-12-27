397e4ba4abded541dc857a324df3ba11285d8680
hyunparrot <coyh1071@gmail.com>
2023-12-27 22:26:31 +0900

    디테일 페이지 위젯 버튼 색바꾸기 작업, 디테일 페이지 위젯, 뒤로가기 설정

--- appbasicSNSteamproj/app/src/main/java/com/example/appbasic_sns_teamproj/DetailPageActivity.kt
+++ appbasicSNSteamproj/app/src/main/java/com/example/appbasic_sns_teamproj/DetailPageActivity.kt
@@ -1,11 +1,23 @@
 package com.example.appbasic_sns_teamproj
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class DetailPageActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail_page)
+
+        val btnBack: Button = findViewById(R.id.btnBack)
+
+        btnBack.setOnClickListener {
+            // 뒤로가기 클릭시 메인으로 이동
+            val intent = Intent(this@DetailPageActivity, MainPageActivity::class.java)
+            startActivity(intent)
+            MainPageActivity()
+        }
     }
 }
