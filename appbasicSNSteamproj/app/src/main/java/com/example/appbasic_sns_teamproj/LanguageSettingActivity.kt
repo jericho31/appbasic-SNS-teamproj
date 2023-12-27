9dd832f0eded8604e0f740719e7d89564a8d8c32
kiwon kim <rlarldnjs17@gmail.com>
2023-12-27 15:45:03 +0900

    코드 리뷰 수정

--- appbasicSNSteamproj/app/src/main/java/com/example/appbasic_sns_teamproj/LanguageSettingActivity.kt
+++ appbasicSNSteamproj/app/src/main/java/com/example/appbasic_sns_teamproj/LanguageSettingActivity.kt
@@ -3,15 +3,18 @@ package com.example.appbasic_sns_teamproj
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
+import android.widget.ImageView
 import android.widget.RadioButton
+import androidx.lifecycle.findViewTreeViewModelStoreOwner
 
 class LanguageSettingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_language_setting)
 
-        val back = findViewById<ImageButton>(R.id.ibtn_back)
+        val back = findViewById<ImageView>(R.id.iv_back)
         val korean = findViewById<RadioButton>(R.id.rbtn_korean)
+        val english = findViewById<RadioButton>(R.id.rbtn_english)
 
         back.setOnClickListener {
             finish()
