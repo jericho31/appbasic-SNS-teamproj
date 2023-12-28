07a22a2e589d1ed1eec0acc4ee1a9ed23b9e4b74
hyunparrot <coyh1071@gmail.com>
2023-12-28 14:04:12 +0900

    디테일 페이지 버튼intent 작업, 디테일과 메인 UI 작업, 메인 draw작업

--- appbasicSNSteamproj/app/src/main/java/com/example/appbasic_sns_teamproj/MainPageActivity.kt
+++ appbasicSNSteamproj/app/src/main/java/com/example/appbasic_sns_teamproj/MainPageActivity.kt
@@ -1,18 +1,22 @@
 package com.example.appbasic_sns_teamproj
 
 import android.content.Intent
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import com.example.appbasic_sns_teamproj.R.layout.activity_main_page
 
 
 class MainPageActivity : AppCompatActivity() {
-
+    private lateinit var apptrack: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(activity_main_page)
 
+        apptrack = findViewById(R.id.appTrack)
         /*
         val logInButton:Button = findViewById(R.id.logInButton)
         logInButton.setOnClickListener {
@@ -20,14 +24,26 @@ class MainPageActivity : AppCompatActivity() {
             startActivity(intent)
         }
     */
-        val btnDetail: Button = findViewById(R.id.btnDetail)
 
+
+        val btnDetail: Button = findViewById(R.id.btnDetail)
         btnDetail.setOnClickListener {
+            detail()
             // 글 클릭시 디테일로 이동
-            val intent = Intent(this@MainPageActivity, DetailPageActivity::class.java)
-            startActivity(intent)
-            DetailPageActivity()
+
+
         }
     }
-}
+    private fun detail() {
+        val appTrackText: String = apptrack.text.toString()
+
+        val intent = Intent(this@MainPageActivity, DetailPageActivity::class.java)
+        intent.putExtra("username", appTrackText)
+
+//        intent.putExtra("backgroundColor", appTrackText.background as? ColorDrawable)
+        //색깔intent
+        startActivity(intent)
+        }
+    }
+
 
