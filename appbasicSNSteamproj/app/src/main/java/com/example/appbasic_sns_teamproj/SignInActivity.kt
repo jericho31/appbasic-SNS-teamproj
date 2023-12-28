63629f35f2e6ab591be66b544d446c80e9ca6e52
OhSeongWon <osuing1243@daum.net>
2023-12-28 11:39:19 +0900

    strings.xml 이름 통일, layout에 있는 이름 strings.xml로 이동

--- appbasicSNSteamproj/app/src/main/java/com/example/appbasic_sns_teamproj/SignInActivity.kt
+++ appbasicSNSteamproj/app/src/main/java/com/example/appbasic_sns_teamproj/SignInActivity.kt
@@ -11,15 +11,13 @@ import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 
 class SignInActivity : AppCompatActivity() {
-    private lateinit var registerLauncher : ActivityResultLauncher<Intent>
-    private lateinit var editTv_id : EditText
-    private lateinit var editTv_pw : EditText
+    private lateinit var registerLauncher: ActivityResultLauncher<Intent>
+    private lateinit var editTv_id: EditText
+    private lateinit var editTv_pw: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
-
-
 
 
         val btn_goSignUpActivity = findViewById<Button>(R.id.btn_goSignUpActivity)
@@ -35,31 +33,30 @@ class SignInActivity : AppCompatActivity() {
 
 
         val btn_logIn = findViewById<Button>(R.id.btn_logIn)
-         btn_logIn.setOnClickListener {
-             if(editTv_id.text.isEmpty()){
-                 Toast.makeText(this, "아이디를 확인해주세요", Toast.LENGTH_SHORT).show()
-             }
-             else if (editTv_pw.text.isEmpty()){
-                 Toast.makeText(this, "패스워드를 확인해주세요", Toast.LENGTH_SHORT).show()
-             }
-             else{
-                 Toast.makeText(this, "환영합니다", Toast.LENGTH_SHORT).show()
-                 val intent = Intent(this, MainPageActivity::class.java)
-                 registerLauncher.launch(intent)
-             }
-         }
-    }
-    fun registerForActivityResult(){
-        registerLauncher =registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
-            result ->
-            if(result.resultCode == RESULT_OK){
-                var idData =result.data?.getStringExtra(Extra.id)
-                var pwData =result.data?.getStringExtra(Extra.password)
-//                Log.d("ASDFFFF", "id = ${Extra.id}")
-//                Log.d("ASDFFFF", "pw = ${Extra.password}")
-                editTv_id.setText(idData)
-                editTv_pw.setText(pwData)
+        btn_logIn.setOnClickListener {
+            if (editTv_id.text.isEmpty()) {
+                Toast.makeText(this, "아이디를 확인해주세요", Toast.LENGTH_SHORT).show()
+            } else if (editTv_pw.text.isEmpty()) {
+                Toast.makeText(this, "패스워드를 확인해주세요", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "환영합니다", Toast.LENGTH_SHORT).show()
+                val intent = Intent(this, MainPageActivity::class.java)
+                registerLauncher.launch(intent)
             }
         }
+    }
+
+    fun registerForActivityResult() {
+        registerLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+                if (result.resultCode == RESULT_OK) {
+                    var idData = result.data?.getStringExtra(Extra.id)
+                    var pwData = result.data?.getStringExtra(Extra.password)
+//                Log.d("ASDFFFF", "id = ${Extra.id}")
+//                Log.d("ASDFFFF", "pw = ${Extra.password}")
+                    editTv_id.setText(idData)
+                    editTv_pw.setText(pwData)
+                }
+            }
     }
 }
