8fd2c3b8ce90b43e4d08784fcd08909026fae5dd
jericho31 <a@a.a>
2023-12-28 15:23:27 +0900

    setSlide 수정

--- appbasicSNSteamproj/app/src/main/java/com/example/appbasic_sns_teamproj/Extensions.kt
+++ appbasicSNSteamproj/app/src/main/java/com/example/appbasic_sns_teamproj/Extensions.kt
@@ -6,12 +6,22 @@ enum class Direction {
     STAY, LEFT, UP, RIGHT, DOWN
 }
 
-fun Activity.setSlide(direction: Direction) {
-    when (direction) {
-        Direction.STAY -> overridePendingTransition(R.anim.stay, R.anim.stay)
-        Direction.LEFT -> overridePendingTransition(R.anim.from_left_enter, R.anim.to_left_exit)
-        Direction.UP -> overridePendingTransition(R.anim.from_up_enter, R.anim.to_up_exit)
-        Direction.RIGHT -> overridePendingTransition(R.anim.from_right_enter, R.anim.to_right_exit)
-        Direction.DOWN -> overridePendingTransition(R.anim.from_down_enter, R.anim.to_down_exit)
+fun Activity.setSlide(enterDirection: Direction, exitDirection: Direction) {
+    var enter = 0
+    var exit = 0
+    enter = when (enterDirection) {
+        Direction.STAY -> R.anim.stay
+        Direction.LEFT -> R.anim.from_left_enter
+        Direction.UP -> R.anim.from_up_enter
+        Direction.RIGHT -> R.anim.from_right_enter
+        Direction.DOWN -> R.anim.from_down_enter
     }
+    exit = when (exitDirection) {
+        Direction.STAY -> R.anim.stay
+        Direction.LEFT -> R.anim.to_left_exit
+        Direction.UP -> R.anim.to_up_exit
+        Direction.RIGHT -> R.anim.to_right_exit
+        Direction.DOWN -> R.anim.to_down_exit
+    }
+    overridePendingTransition(enter, exit)
 }
