0f6fdc1dabccf153cfab25576f5dcca0f00127f8
hyunparrot <coyh1071@gmail.com>
2023-12-28 23:55:45 +0900

    Globar.kt 와 MainPageActivity.kt 를 수정하여 로그인과 비로그인 일때,  각자 프로필 사진을 눌렀을때 다르게 페이지가 열리도록 수정중(미완성)

--- appbasicSNSteamproj/app/src/main/java/com/example/appbasic_sns_teamproj/Global.kt
+++ appbasicSNSteamproj/app/src/main/java/com/example/appbasic_sns_teamproj/Global.kt
@@ -1,14 +1,54 @@
 package com.example.appbasic_sns_teamproj
 
+import android.content.Context
+import android.content.Intent
+import android.widget.ImageView
+
 /** intent extra name 용 */
 // 오타로 인한 휴먼 에러를 줄일 수 있다고 함.
+//Extra가 겹쳐서 1로 바꿈.
+
 object Extra {
     const val id = "id"
     const val password = "password"
+
+}
+object Global {
+
+
+    object User {
+        var isSignedIn = false
+        var id = ""
+        var isKorean = true
+    }
+
+ fun profileChoice(profileImageView: ImageView, context: Context) {
+        if (User.isSignedIn) {
+            // 로그인 상태일 때
+            // 로그인 상태에 따른 아이콘/프로필 이미지 설정
+            // 예시: 이미지 리소스를 R.drawable.xxx 로 설정
+            profileImageView.setImageResource(R.drawable.snake_sparta)
+
+            // 프로필 이미지 클릭 시 마이페이지로 이동
+            profileImageView.setOnClickListener {
+                val intent = Intent(context, MyPageActivity::class.java)
+                context.startActivity(intent)
+            }
+        } else {
+            // 비로그인 상태일 때
+            // 비로그인 상태에 따른 아이콘/프로필 이미지 설정
+            // 예시: 이미지 리소스를 R.drawable.yyy 로 설정
+            profileImageView.setImageResource(R.drawable.ic_sparta)
+
+            // 프로필 이미지 클릭 시 로그인 페이지로 이동
+            profileImageView.setOnClickListener {
+                val intent = Intent(context, SignInActivity::class.java)
+                context.startActivity(intent)
+            }
+        }
+    }
 }
 
-object User {
-    var isSignedIn = false
-    var id = ""
-    var isKorean = true
-}
+//로그인 을해도 똑같이 로그인페이지로 넘어감.
+
+
--- appbasicSNSteamproj/app/src/main/java/com/example/appbasic_sns_teamproj/MainPageActivity.kt
+++ appbasicSNSteamproj/app/src/main/java/com/example/appbasic_sns_teamproj/MainPageActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.ImageView
@@ -19,8 +20,34 @@ class MainPageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(activity_main_page)
+//        현재 액티비티의 상태를 복원하고 레이아웃 파일인 activity_main_page를 화면에 표시하는 코드이다.
 
-        appTrack = findViewById(R.id.appTrack)
+
+                appTrack = findViewById(R.id.appTrack)
+        val myProfile = findViewById<ImageButton>(R.id.myProfile)
+//        appTrack 변수에 ID가 appTrack인 TextView를 할당하고,
+//        myProfile 변수에 ID가 myProfile인 ImageButton를 할당한다.
+
+// 로그인 여부에 따라 프로필 이미지 업데이트 및 클릭 액션 설정
+                Global.profileChoice(myProfile, this)
+//        Global 클래스의 profileChoice 메서드를 호출하여,
+//        로그인 여부에 따라 프로필 이미지를 업데이트하고 클릭 액션을 설정한다.
+//        myProfile은 프로필 이미지를 나타내는 ImageButton이며,
+//        this는 현재 액티비티를 나타냅니다.
+
+
+        val btnDetail: Button = findViewById(R.id.btnDetail)
+        btnDetail.setOnClickListener {
+            val appTrackText: String = appTrack.text.toString()
+            val intent = Intent(this@MainPageActivity, DetailPageActivity::class.java)
+            intent.putExtra("username", appTrackText)
+            startActivity(intent)
+        }
+//        ID가 btnDetail인 Button을 찾아 btnDetail 변수에 할당하고,
+    //        해당 버튼에 클릭 리스너를 설정한다. 버튼이 클릭되면
+    //        DetailPageActivity로 이동하면서
+    //        username이라는 키로 appTrackText 값을 전달한다.
+
 
         /*
         val logInButton:Button = findViewById(R.id.logInButton)
@@ -35,30 +62,19 @@ class MainPageActivity : AppCompatActivity() {
             startActivity(intent)
         }
     */
+//        val myProfile: ImageButton = findViewById(R.id.myProfile)
+//        myProfile.setOnClickListener {
+//    val intent = Intent(this, SignInActivity::class.java)
+//    startActivity(intent)
+//}
 
-        val myProfile: ImageButton = findViewById(R.id.myProfile)
-        myProfile.setOnClickListener {
-            val intent = Intent(this, SignInActivity::class.java)
-            startActivity(intent)
-        }
 //        val myProfile: ImageButton = findViewById(R.id.myProfile)
 //        myProfile.setOnClickListener {
 //            val intent = Intent(this, MyPageActivity::class.java)
 //            startActivity(intent)
 //        }
 
-        val btnDetail: Button = findViewById(R.id.btnDetail)
-        btnDetail.setOnClickListener {
-
-            val appTrackText: String = appTrack.text.toString()
-
-            val intent = Intent(this@MainPageActivity, DetailPageActivity::class.java)
-            intent.putExtra("username", appTrackText)
-
-//        intent.putExtra("backgroundColor", appTrackText.background as? ColorDrawable)
-            //색깔intent
-            startActivity(intent)
-
-        }
     }
 }
+
+
