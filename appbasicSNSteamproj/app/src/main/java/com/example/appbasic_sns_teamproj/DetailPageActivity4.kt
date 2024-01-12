529f2da2be8ce31903069bb621e0c4330deeb266
jericho31 <a@a.a>
2024-01-12 15:43:39 +0900

    fix - 주석 작업

--- appbasicSNSteamproj/app/src/main/java/com/example/appbasic_sns_teamproj/DetailPageActivity4.kt
+++ appbasicSNSteamproj/app/src/main/java/com/example/appbasic_sns_teamproj/DetailPageActivity4.kt
@@ -20,6 +20,7 @@ class DetailPageActivity4 : AppCompatActivity() {
         setContentView(R.layout.activity_detail_page4)
         //asdf
         // aa2
+        // jericho work
         val btnBack: Button = findViewById(R.id.btnBack)
         val dtAiTrack: TextView = findViewById(R.id.dtAiTrack)
         val buttonLike = findViewById<Button>(R.id.buttonLike)
